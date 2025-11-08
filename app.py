365b93349b5ba3a4e7b6de55f9b79eedb1ecdb2b
rileycrandall04 <riley.crandall@gmail.com>
2025-11-08 10:58:14 -0700

    Update app.py

--- app.py
+++ app.py
@@ -405,7 +405,7 @@ try:
         
         st.subheader("Add Entry")
     
-    date = st.date_input("Date", value=dt.date.today(), format="MM/DD/YYYY")
+        date = st.date_input("Date", value=dt.date.today(), format="MM/DD/YYYY")
     
         date = st.date_input("Date", dt.date.today(), format="MM/DD/YYYY")
     
